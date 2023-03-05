b642966ad8f216d09a97dd044fa45758f98457d0
ziard47 <mohomedziard5@gmail.com>
2023-03-05 15:22:01 +0530

    payment Completed

---
+++ src/payment.java
@@ -0,0 +1,8 @@
+public class payment {
+    public static void main(String... args) {
+
+        System.out.println("Payement type");
+        System.out.println("Dater");
+        System.out.println("Cost");
+    }
+}
