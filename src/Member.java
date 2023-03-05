8f778577aaf598d3ee55ad60babc948242deb879
ziard47 <mohomedziard5@gmail.com>
2023-03-05 12:07:44 +0530

    Member reg Completed

---
+++ src/Member.java
@@ -0,0 +1,8 @@
+public class Member {
+    public static void main(String... args){
+        System.out.println("Member Name");
+        System.out.println("Address");
+        System.out.println("Date of birth");
+        System.out.println("NIC");
+    }
+}
